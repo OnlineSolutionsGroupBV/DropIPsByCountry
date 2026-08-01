cbd91eb7e46f081a9dfbc4ed59bf2b77c606627e
Jobsite Server <info@nieuwejobs.com>
2026-08-01 14:21:30 +0200

    Updated scripts for python2

---
+++ run.bash
@@ -0,0 +1,7 @@
+./run_prepare_generiek_blocks.sh
+
+#python parse_ips.py
+#python get_ip_country.py
+#python aggregate_generiek_subnets.py
+#python block_generiek_subnet.py 
+#./run_clean_crawlers_py2.sh
